ef8d9a0a230a8b4bfa3ff73f65e7d262e0b964cd
Joel Whitney <whitney.joel.b@gmail.com>
2020-02-18 11:02:03 -0500

    3and5s and Fibonaccis

---
+++ problems/even_fibonacci/even_fibonacci.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+let maxTermValue = 4000000
+
+func sumOfEvens(_ maxTermValue: Int) -> Int {
+    var sum = 0
+    findEvenFibonacciTerms(maxTermValue).forEach { multiple in
+        sum += multiple
+    }
+    return sum
+}
+
+func findEvenFibonacciTerms(_ maxTermValue: Int) -> [Int] {
+    var fibonaccis = [1, 2]
+
+    while (fibonaccis.last!) < maxTermValue {
+        let lastTwoTerms = fibonaccis[(fibonaccis.count - 2)...]
+        let sumOfLastTwoTerms = lastTwoTerms.first! + lastTwoTerms.last!
+        if sumOfLastTwoTerms <= maxTermValue {
+            fibonaccis.append(sumOfLastTwoTerms)
+        } else {
+            break
+        }
+    }
+    return fibonaccis.filter { $0 % 2 == 0 }
+}
+
+sumOfEvens(maxTermValue)
